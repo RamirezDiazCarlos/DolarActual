4e3d33f91cfcb278c98f4c37798b03be5b144448
RDCarlos <cerd469@gmial.com>
2025-04-18 20:59:28 -0300

    rutas

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
         <nav className="navbar navbar-dark bg-dark">
             <div className="container d-flex justify-content-center align-items-center">
                 <a className="navbar-brand d-flex align-items-center gap-2" href="/">
-                    <img src="../../public/logo.png" width="106" alt="DolarActual" />
+                    <img src="/logo.png" width="106" alt="DolarActual" />
                 </a>
             </div>
         </nav>
