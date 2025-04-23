5dec173c5dc6160461cf2e5fa5527bffda2ce735
RDCarlos <cerd469@gmail.com>
2025-04-22 21:19:23 -0300

    helmet

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet } from "react-helmet";
 import './App.css'
 import Inflacion from "./components/Inflacion";
 import Conversor from "./components/Conversor";
@@ -16,8 +17,21 @@ function App() {
 
   return (
     <div className="justify-content-center gradient-bg">
+      <Helmet>
+        <title>Cotizador de Dólar Actual</title>
+        <meta name="description" content="Consulta el valor actualizado del dólar, gráficos históricos y conversor de divisas en Argentina." />
+        <meta name="keywords" content="dólar, cotización, argentina, blue, oficial, mep, contado con liqui, conversor, inflación" />
+        <meta name="author" content="Tu Nombre o Proyecto" />
+        <meta property="og:title" content="Cotizador de Dólar Actual" />
+        <meta property="og:description" content="Cotizaciones actualizadas, gráficos históricos y conversor de divisas en Argentina." />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Cotizador de Dólar Actual" />
+        <meta name="twitter:description" content="Cotizaciones actualizadas, gráficos históricos y conversor de divisas en Argentina." />
+        <link rel="icon" href="/DolarActual/public/logo.png" />
+      </Helmet>
       <Navbar />
-      <Conversor/>
+      <Conversor />
       <ListaDolares dolares={dolares} />
       <Inflacion />
       <Footer />
